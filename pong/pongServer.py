775c7588c14f1eefa3156aec25b156363528fbbc
Raycharr <raycharr@gmail.com>
2023-11-07 01:08:36 -0500

    Update pongServer.py
    Created a skeleton of a client handler function to pass as the argument for threads.

--- pong/pongServer.py
+++ pong/pongServer.py
@@ -9,14 +9,25 @@
 import socket
 import threading
 
-
-
 # Use this file to write your server logic
 # You will need to support at least two clients
 # You will need to keep track of where on the screen (x,y coordinates) each paddle is, the score 
 # for each player and where the ball is, and relay that to each client
 # I suggest you use the sync variable in pongClient.py to determine how out of sync your two
 # clients are and take actions to resync the games
+
+def client_handler(currClient, addr):
+    #main loop for handling connected clients
+    while True:
+        msg = currClient.recv(1024)
+        if msg == 'something weird':
+            break
+        #recieve packet from client
+        #check sync
+        #do other things
+    #close connection
+    currClient.close()
+
 
 server = socket.socket(socket.AF_INET, socket.SOCK_STREAM)      # Creating the server
 
